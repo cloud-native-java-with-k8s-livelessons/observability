af2885403386af59bca3413066f78cd31883d20e
Josh Long <josh@joshlong.com>
2021-09-21 15:25:30 -0700

    admin

--- spring-boot-admin/server/src/main/java/com/example/server/ServerApplication.java
+++ spring-boot-admin/server/src/main/java/com/example/server/ServerApplication.java
@@ -1,19 +1,8 @@
 package com.example.server;
 
-import de.codecentric.boot.admin.server.config.AdminServerProperties;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.authentication.SavedRequestAwareAuthenticationSuccessHandler;
-import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-
-import java.util.UUID;
 
 /**
 	* https://www.baeldung.com/spring-boot-admin
