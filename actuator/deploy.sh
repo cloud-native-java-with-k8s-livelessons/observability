824a0045e105e1688ae53eb48ef2ba22124af561
Josh Long <josh@joshlong.com>
2021-09-21 13:40:21 -0700

    observability

--- actuator/deploy.sh
+++ actuator/deploy.sh
@@ -8,95 +8,12 @@ NS=cnj
 APP_NAME=actuator
 MANIFESTS_DIR=$HERE/k8s/manifests
 IMAGE_NAME=gcr.io/bootiful/cnj-${APP_NAME}:latest
+mkdir -p $MANIFESTS_DIR
+kubectl delete ns/cnj
 ./mvnw -DskipTests=true clean package spring-boot:build-image -Dspring-boot.build-image.imageName=${IMAGE_NAME}
 docker push $IMAGE_NAME
-mkdir -p $MANIFESTS_DIR
-
-kubectl apply -f $MANIFESTS_DIR
-kubectl -n $NS port-forward deployment/$APP_NAME 8080:8080
-
-#kubectl create ns $NS  -o yaml > $MANIFESTS_DIR/namespace.yaml
-#kubectl -n $NS create deployment  --image=$IMAGE_NAME $APP_NAME -o yaml > $MANIFESTS_DIR/deployment.yaml
-#kubectl -n $NS expose deployment $APP_NAME --port=8080 -o yaml > $MANIFESTS_DIR/service.yaml
-#sleep 5
-#kubectl -n $NS port-forward deployment/$APP_NAME 8080:8080  &
-
-curl -s localhost:8080/actuator/health/liveness | jq
-
-kubectl logs -n cnj deployments/$APP_NAME
-
-# 1. test readiness / liveness actuators
-
-#     ```bash
-#     curl -s localhost:8080/actuator/health | jq
-#     curl -s localhost:8080/actuator/health/readiness | jq
-#     curl -s localhost:8080/actuator/health/liveness | jq
-#     ```
-
-# 1. configure readiness / liveness probes (edit k8s/manifests/deployment.yaml)
-
-#     ```yaml
-#     spec:
-#       terminationGracePeriodSeconds: 30
-#     containers:
-#     ...
-#         env:
-#           - name: MANAGEMENT_SERVER_PORT
-#             value: "9001"
-#         livenessProbe:
-#           httpGet:
-#             path: /actuator/health/liveness
-#             port: 9001
-#         readinessProbe:
-#           httpGet:
-#             path: /actuator/health/readiness
-#             port: 9001
-#     ```
-
-# 1. apply the new changes
-
-#     ```bash
-#     kubectl -n booternetes apply -f k8s/manifests/deployment.yaml
-#     ```
-
-
-# 1. Deploy a ingress ... WOW MAGIC DNS / TLS
-
-#     ```bash
-#      kubectl -n booternetes create ingress customer --class=default \
-#       --rule="crm.s1t.k8s.camp/*=customer:8080,tls=crm-secret" \
-#       --annotation="cert-manager.io/cluster-issuer=letsencrypt-prod" \
-#       -o yaml > k8s/manifests/ingress.yaml
-#     ```
-
-# ### Deploy with a database
-
-# 1. Configure to use a database by reading the cloud properties into a secret:
-
-#     ```bash
-#     kubectl -n booternetes create secret generic customers --from-file ./src/main/resources/application-cloud.properties
-#     ```
-
-# 1. update kube deployment to mount secret as files...
-
-#     ```bash
-#         spec:
-#           containers:
-#           ...
-#             env:
-#               - name: SPRING_PROFILES_ACTIVE
-#                 value: cloud
-#             volumeMounts:
-#               - mountPath: "/config"
-#                 name: config
-#                 readOnly: true
-#           volumes:
-#             - name: config
-#               secret:
-#                 secretName: customers
-#     ```
-
-
-# ## Scripts
-
-# I've put `run.sh` and `build.sh` in the `customers` module. Invoke `build.sh` and then `run.sh`.
+sleep 5
+kubectl apply -f "$MANIFESTS_DIR/namespace.yaml"
+kubectl apply -f "$MANIFESTS_DIR/deployment.yaml"
+kubectl apply -f "$MANIFESTS_DIR/service.yaml"
+sleep 5
--- actuator/src/main/java/cnj/actuator/ActuatorApplication.java
+++ actuator/src/main/java/cnj/actuator/ActuatorApplication.java
@@ -1,10 +1,17 @@
 package cnj.actuator;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.MeterRegistry;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.availability.AvailabilityChangeEvent;
-import org.springframework.boot.availability.ReadinessState;
+import org.springframework.boot.availability.AvailabilityState;
+import org.springframework.boot.availability.LivenessState;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -12,25 +19,61 @@ import reactor.core.publisher.Mono;
 
 import java.util.Map;
 
-@Controller
-@ResponseBody
 @SpringBootApplication
 public class ActuatorApplication {
 
-    private final ApplicationContext applicationContext;
+	@Bean
+	HealthIndicator bootifulHealthIndicator() {
+		return () -> Health.status("I <3 Production!").build();
+	}
 
-    ActuatorApplication(ApplicationContext applicationContext) {
-        this.applicationContext = applicationContext;
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(ActuatorApplication.class, args);
+	}
+}
 
-    @PostMapping
-    Mono<Map<String, ReadinessState>> post() {
-        AvailabilityChangeEvent.publish(this.applicationContext, ReadinessState.REFUSING_TRAFFIC);
-        return Mono.just(Map.of("status", ReadinessState.REFUSING_TRAFFIC));
-    }
 
-    public static void main(String[] args) {
-        SpringApplication.run(ActuatorApplication.class, args);
-    }
+@Controller
+@ResponseBody
+class MetricsController {
 
+	private final MeterRegistry registry;
+	private final Counter counter;
+
+	MetricsController(MeterRegistry registry) {
+		this.registry = registry;
+		this.counter = this.registry.counter("demo-counter");
+	}
+
+	@PostMapping("/increment")
+	void increment() {
+		this.counter.increment();
+	}
+}
+
+@Controller
+@ResponseBody
+class HealthRestController {
+
+	private final ApplicationContext applicationContext;
+
+	HealthRestController(
+		ObjectProvider<AvailabilityState> availabilityStates,
+		ApplicationContext applicationContext) {
+		this.applicationContext = applicationContext;
+	}
+
+	@PostMapping("/green")
+	Mono<Map<String, LivenessState>> green() {
+		var state = LivenessState.CORRECT;
+		AvailabilityChangeEvent.publish(this.applicationContext, state);
+		return Mono.just(Map.of("status", state));
+	}
+
+	@PostMapping("/red")
+	Mono<Map<String, LivenessState>> red() {
+		var state = LivenessState.BROKEN;
+		AvailabilityChangeEvent.publish(this.applicationContext, state);
+		return Mono.just(Map.of("status", state));
+	}
 }
